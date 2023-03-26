acb543b1b990637dc84984931d2bbf9814392e56
Marlon Velasco <marvelas77@gmail.com>
2023-03-26 18:13:36 -0500

    Added 'GravyStack' Editor menu to easily add new Character and Conversation scriptable objects

---
+++ Assets/Editor/MakeNewCharacter.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEngine;
+
+public class MakeNewCharacter
+{
+	[MenuItem("GravyStack/Make New Character")]
+	public static void CreateMyAsset()
+	{
+		Character asset = ScriptableObject.CreateInstance<Character>();
+
+		AssetDatabase.CreateAsset(asset, "Assets/Characters/NewCharacterData.asset");
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+
+		Selection.activeObject = asset;
+	}
+}
---
+++ Assets/Editor/MakeNewConversation.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEngine;
+
+public class MakeNewConversation
+{
+	[MenuItem("GravyStack/Make New Conversation")]
+	public static void CreateMyAsset()
+	{
+		Conversation asset = ScriptableObject.CreateInstance<Conversation>();
+
+		AssetDatabase.CreateAsset(asset, "Assets/Conversations/NewConversationData.asset");
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+
+		Selection.activeObject = asset;
+	}
+}
